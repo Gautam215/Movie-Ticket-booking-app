baf58800473249cfa512f6fe939fb2ba1adb6eea
Abhishek Kumar Gautam <ak9724068@gmail.com>
2026-09-23 10:50:48 +0530

    Deploy Eventra client-config files

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class'],
+  corePlugins: { preflight: false },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
